e463af8407663529a85d2ea3e5bf101729fe0cc6
Davidpuente12 <luissda1995@gmail.com>
2026-04-20 17:00:15 -0500

    agg scroll

--- diseño-responsivo/responsivo.js
+++ diseño-responsivo/responsivo.js
@@ -1,44 +1,54 @@
 function cambiarAncho(accion) {
-    const caja = document.getElementById("content-responsive");
-    let anchoActual = parseInt(window.getComputedStyle(caja).width);
+  const caja = document.getElementById("content-responsive");
+  let anchoActual = parseInt(window.getComputedStyle(caja).width);
 
-    if (accion == "aumentar") {
-        caja.style.width = (anchoActual + 30) + "px";
-    }else if (accion == "reducir") {
-        caja.style.width = Math.max(anchoActual - 30, 30) + "px";
-    }
+  if (accion == "aumentar") {
+    caja.style.width = anchoActual + 30 + "px";
+  } else if (accion == "reducir") {
+    caja.style.width = Math.max(anchoActual - 30, 30) + "px";
+  }
 }
 
-//Botones media 
-const tablaMedia = document.getElementById("tablaMedia")
-const tablaTipo = document.getElementById("tablaTipo")
-const tablaCondicion = document.getElementById("tablaCondicion")
+//Botones media
+const tablaMedia = document.getElementById("tablaMedia");
+const tablaTipo = document.getElementById("tablaTipo");
+const tablaCondicion = document.getElementById("tablaCondicion");
 
-function sintaxisMedia(accion){
-    if(accion == "media"){
-        tablaMedia.style.display = "block"
-        tablaTipo.style.display = "none"
-        tablaCondicion.style.display = "none"
-    }else if(accion == "tipos"){
-        tablaTipo.style.display = "block"
-        tablaMedia.style.display = "none"
-        tablaCondicion.style.display = "none"
-    }else if(accion == "condicion"){
-        tablaCondicion.style.display = "block"
-        tablaMedia.style.display = "none"
-        tablaTipo.style.display = "none"
-    }
+function sintaxisMedia(accion) {
+  if (accion == "media") {
+    tablaMedia.style.display = "block";
+    tablaTipo.style.display = "none";
+    tablaCondicion.style.display = "none";
+  } else if (accion == "tipos") {
+    tablaTipo.style.display = "block";
+    tablaMedia.style.display = "none";
+    tablaCondicion.style.display = "none";
+  } else if (accion == "condicion") {
+    tablaCondicion.style.display = "block";
+    tablaMedia.style.display = "none";
+    tablaTipo.style.display = "none";
+  }
 }
 
 //Patrones de Diseño
-function abrirVentana(accion){
-
-    if(accion == "Mustly"){
-        window.open("Patrones-Diseño/Mustly-Fluid/mustly.html", "popup", "width=500, height=700")
-    }else if(accion == "Shifter"){
-        window.open("Patrones-Diseño/Layout-Shifter/shifter.html", "popup", "width=500, height=700")
-    }else if(accion == "Drop"){
-        window.open("Patrones-Diseño/Column-Drop/drop.html", "popup", "width=500, height=700")
-    }
-    
+function abrirVentana(accion) {
+  if (accion == "Mustly") {
+    window.open(
+      "Patrones-Diseño/Mustly-Fluid/mustly.html",
+      "popup",
+      "width=500, height=700",
+    );
+  } else if (accion == "Shifter") {
+    window.open(
+      "Patrones-Diseño/Layout-Shifter/shifter.html",
+      "popup",
+      "width=500, height=700",
+    );
+  } else if (accion == "Drop") {
+    window.open(
+      "Patrones-Diseño/Column-Drop/drop.html",
+      "popup",
+      "width=500, height=700",
+    );
+  }
 }
--- pseudoClases/pseudoClases.js
+++ pseudoClases/pseudoClases.js
@@ -1,12 +1,121 @@
 /////////////////// PSEUDO CLASES
-const botones_nth_child = document.querySelectorAll('.botones-nth-child button')
 
-function renderPseudoEstructuras(tipo){
-    const codigo = document.querySelector('#codigo-nth')
+// hover
+document
+  .querySelector(".btn-formulario-hover")
+  .addEventListener("click", () => {
+    ejecutarPseudoClasesInteraccion("formulario");
+  });
+document
+  .querySelector(".btn-menuInteractivo-hover")
+  .addEventListener("click", () => {
+    ejecutarPseudoClasesInteraccion("menu-interactivo");
+  });
 
-    if(tipo === 'render-nth-child'){
-        codigo.className = 'botones-nth botones-nth-child'
-        codigo.innerHTML = `
+function ejecutarPseudoClasesInteraccion(tipo) {
+  const codigo = document.getElementById("codigo-pseudoClasesInteraccion");
+  const demo = document.getElementById("demo-pseudoClasesInteraccion");
+
+  if (tipo === "formulario") {
+    codigo.innerHTML = `
+    <p style="margin-top: 0">
+      <b class="golden">input:focus</b>{ <br />
+      &emsp; <b class="sky">color</b>: rgb(254, 85, 51); <br />
+      &emsp; <b class="sky">background</b>: #383838; <br />
+      }
+    </p>
+    <p>
+      <b class="golden">div:focus-within</b>{
+      <b class="sky">background</b>:rgb(254, 85, 51);} <br />
+    </p>
+    <p>
+      <b class="golden">button:hover</b>{ <b class="sky">font-size</b>:
+      2rem;} <br />
+    </p>
+    <p>
+      <b class="golden">button:active</b>{ <b class="sky">color</b>:
+      rgb(0, 98, 255);}
+    </p>
+    `;
+
+    demo.innerHTML = `
+    <label for="label-ejemplofocus">Nombre</label>
+    <input
+      type="text"
+      class="elemento-ejemplofocus"
+      id="label-ejemplofocus"
+      value="Hacer Click"
+    />
+
+    <div id="focuswithin">
+      <label for="label-ejemplofocusWithin">Apellido</label>
+      <input
+        type="text"
+        id="label-ejemplofocusWithin"
+        value="Hacer Click"
+        class="elemento-ejemplofocus"
+      />
+    </div>
+    <button id="elemento-ejemplo-hoverYactive">Enviar</button>
+    `;
+  } else if (tipo === "menu-interactivo") {
+    codigo.innerHTML = `
+    <p>
+      <b class="golden">#boton</b> { <br />
+      &emsp; cursor: pointer; <br />
+      }
+    </p>
+    <div>
+      <b class="golden">#menu-list</b> { <br />
+      <p class="margenIz margincero">
+        width: 150px; <br />
+        <b class="sky">opacity</b>: 0; <br />
+        <b class="sky">transform</b>:
+        <b class="amarillo2">scaleY</b>(0); <br />
+        <b class="sky">transform-origin</b>: <b class="orange">top</b>;
+        <br />
+        <b class="sky">transition</b>: <br />
+        &emsp; opacity 1s, <br />
+        &emsp; transform 1s; <br />
+      </p>
+      }
+    </div>
+    <p>
+      <b class="golden">#boton:hover</b> +
+      <b class="golden">#menu-list</b>, <br />
+      <b class="golden">#boton:focus-within</b> +
+      <b class="golden">#menu-list</b> { <br />
+      &emsp; <b class="sky">opacity</b>: 1; <br />
+      &emsp; <b class="sky">transform</b>:
+      <b class="amarillo2">scaleY</b>(1); <br />
+      }
+    </p>
+    `;
+
+    demo.innerHTML = `
+    <div>
+      <button id="btn-menu-interactivo">Menu</button>
+      <ul id="menu-list">
+        <li>Inicio</li>
+        <li>Servicios</li>
+        <li>Contacto</li>
+      </ul>
+    </div>
+    `;
+  }
+}
+
+// nth-child
+const botones_nth_child = document.querySelectorAll(
+  ".botones-nth-child button",
+);
+
+function renderPseudoEstructuras(tipo) {
+  const codigo = document.querySelector("#codigo-nth");
+
+  if (tipo === "render-nth-child") {
+    codigo.className = "botones-nth botones-nth-child";
+    codigo.innerHTML = `
         <p>
             <b class="amarillo">:nth-child(<b class="verde">5</b>)</b>{
             <b class="sky">background</b>: blue;}
@@ -61,13 +170,14 @@ function renderPseudoEstructuras(tipo){
             <button>:onlychild</button>
             <button>span:onlychild</button>
         </div>
-        `
-        const botones_nth_child = document.querySelectorAll('.botones-nth-child button')
-        return botonesNthChild(botones_nth_child)
-    }
-    else if(tipo === 'render-nth-type'){
-        codigo.className = 'botones-nth botones-nth-type'
-        codigo.innerHTML = `
+        `;
+    const botones_nth_child = document.querySelectorAll(
+      ".botones-nth-child button",
+    );
+    return botonesNthChild(botones_nth_child);
+  } else if (tipo === "render-nth-type") {
+    codigo.className = "botones-nth botones-nth-type";
+    codigo.innerHTML = `
         <p>
             <b class="amarillo">:nth-child(<b class="verde">5</b>)</b>{
             <b class="sky">background</b>: blue;}
@@ -77,94 +187,86 @@ function renderPseudoEstructuras(tipo){
             <button>:nth-of-type(1)</button>
             <button>p:nth-of-type(1)</button>
         </div>
-        `
-        const botones_nth_type = document.querySelectorAll('.botones-nth-type button')
-        return botonesNthType(botones_nth_type)
-    }
-    else if(tipo === 'render-has'){
-        codigo.className = `botones-nth boton-has`
-        codigo.innerHTML = `
+        `;
+    const botones_nth_type = document.querySelectorAll(
+      ".botones-nth-type button",
+    );
+    return botonesNthType(botones_nth_type);
+  } else if (tipo === "render-has") {
+    codigo.className = `botones-nth boton-has`;
+    codigo.innerHTML = `
         <p>
             <b class="amarillo">:has(p)</b>{
             <b class="sky">background</b>: blue;}
         </p>
         <button>:has</button>
-        `
-        const boton_has = document.querySelector('.boton-has button')
-        boton_has.addEventListener('click',() => pseudoClases('has'))
-    }
-
+        `;
+    const boton_has = document.querySelector(".boton-has button");
+    boton_has.addEventListener("click", () => pseudoClases("has"));
+  }
 }
 
-const btn_Controls = document.querySelectorAll('.btn-controls button')
-btn_Controls.forEach((boton,index) => {
-    boton.addEventListener('click', () => {
-        if(index === 0) renderPseudoEstructuras('render-nth-child')
-        else if(index === 1) renderPseudoEstructuras('render-nth-type')
-        else if(index === 2) renderPseudoEstructuras('render-has')
-    })
-})
+const btn_Controls = document.querySelectorAll(".btn-controls button");
+btn_Controls.forEach((boton, index) => {
+  boton.addEventListener("click", () => {
+    if (index === 0) renderPseudoEstructuras("render-nth-child");
+    else if (index === 1) renderPseudoEstructuras("render-nth-type");
+    else if (index === 2) renderPseudoEstructuras("render-has");
+  });
+});
 
-function botonesNthChild(botones){
-
-    botones.forEach((boton, index) => {
-        boton.addEventListener('click', () => {
-            //nth-child(3)
-            if(index === 0) pseudoClases('childn')
-            else if(index === 1) pseudoClases('p-childn')
-            //nth-child(odd)
-            else if(index === 2) pseudoClases('odd')
-            else if(index === 3) pseudoClases('p-odd')
-            //nth-child(even)
-            else if(index === 4) pseudoClases('even')
-            else if(index === 5) pseudoClases('p-even')
-            /* nth-child(3n) */
-            else if(index === 6) pseudoClases('tresn')
-            else if(index === 7) pseudoClases('p-tresn')
-            /* nth-child(3n+2) */
-            else if(index === 8) pseudoClases('dosn')
-            else if(index === 9) pseudoClases('p-dosn')
-            /* nth-child(-n+3) */
-            else if(index === 10) pseudoClases('menostres')
-            else if(index === 11) pseudoClases('p-menostres')
-            /* nth-child(n) */
-            else if(index === 12) pseudoClases('todos')
-            else if(index === 13) pseudoClases('p-todos')
-            /* first-child */
-            else if(index === 14) pseudoClases('firstchild')
-            else if(index === 15) pseudoClases('p-firstchild')
-            /* last-child */
-            else if(index === 16) pseudoClases('lastchild')
-            else if(index === 17) pseudoClases('p-lastchild')
-            /* nth-of-type(n) */
-            else if(index === 18) pseudoClases('onlychild')
-            else if(index === 19) pseudoClases('p-onlychild')
-        })
-    })
+function botonesNthChild(botones) {
+  botones.forEach((boton, index) => {
+    boton.addEventListener("click", () => {
+      //nth-child(3)
+      if (index === 0) pseudoClases("childn");
+      else if (index === 1) pseudoClases("p-childn");
+      //nth-child(odd)
+      else if (index === 2) pseudoClases("odd");
+      else if (index === 3) pseudoClases("p-odd");
+      //nth-child(even)
+      else if (index === 4) pseudoClases("even");
+      else if (index === 5) pseudoClases("p-even");
+      /* nth-child(3n) */ else if (index === 6) pseudoClases("tresn");
+      else if (index === 7) pseudoClases("p-tresn");
+      /* nth-child(3n+2) */ else if (index === 8) pseudoClases("dosn");
+      else if (index === 9) pseudoClases("p-dosn");
+      /* nth-child(-n+3) */ else if (index === 10) pseudoClases("menostres");
+      else if (index === 11) pseudoClases("p-menostres");
+      /* nth-child(n) */ else if (index === 12) pseudoClases("todos");
+      else if (index === 13) pseudoClases("p-todos");
+      /* first-child */ else if (index === 14) pseudoClases("firstchild");
+      else if (index === 15) pseudoClases("p-firstchild");
+      /* last-child */ else if (index === 16) pseudoClases("lastchild");
+      else if (index === 17) pseudoClases("p-lastchild");
+      /* nth-of-type(n) */ else if (index === 18) pseudoClases("onlychild");
+      else if (index === 19) pseudoClases("p-onlychild");
+    });
+  });
 }
-botonesNthChild(botones_nth_child)
+botonesNthChild(botones_nth_child);
 
 function botonesNthType(botones) {
-    botones.forEach((boton, index) => {
-        boton.addEventListener('click', () => {
-            if(index === 0) pseudoClases('nthoftype')
-            else if(index === 1) pseudoClases('p-nthoftype')
-        })
-    })
+  botones.forEach((boton, index) => {
+    boton.addEventListener("click", () => {
+      if (index === 0) pseudoClases("nthoftype");
+      else if (index === 1) pseudoClases("p-nthoftype");
+    });
+  });
 }
 
-const elementos = document.getElementById("Elementos")
-function pseudoClases(tipo){
-    elementos.className = tipo
+const elementos = document.getElementById("Elementos");
+function pseudoClases(tipo) {
+  elementos.className = tipo;
 }
-
-
 
 /////////////////// PSEUDO ELEMENTOS
-const botonesPseudoElementos = document.querySelectorAll("#ejemplo-pseudo-elementos p")
+const botonesPseudoElementos = document.querySelectorAll(
+  "#ejemplo-pseudo-elementos p",
+);
 
-function ejecutarPseudoElementos(tipo){
-    botonesPseudoElementos.forEach(item => {
-        item.classList.toggle(tipo)
-    })
+function ejecutarPseudoElementos(tipo) {
+  botonesPseudoElementos.forEach((item) => {
+    item.classList.toggle(tipo);
+  });
 }
